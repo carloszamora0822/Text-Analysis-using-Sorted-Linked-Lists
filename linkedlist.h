776557e476ff4daffaa591c3f835bbf345fcca6c
Carlos Zamora <165355838+carloszamora0822@users.noreply.github.com>
2024-10-04 13:08:23 -0500

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -9,20 +9,34 @@ class LinkedList
     public:
     LinkedList();
     ~LinkedList();
-    
-    // node manipulation
-    //string words;
-   //LinkedList *next;
-    
-   //void insert_sorted_list(LinkedList* &head, string &words);
-    
 
-
-    // reading file
+    // reading and manipulating words
     void read_txt(ifstream &inputfile);
-    bool isWord(string &word);
+    void insertSorted(const string &word);
+    void printList() const; // error checking
+    void output() const;
 
     private:
+    ifstream inputfile;
+    
+    // outputs
+    int countCleanedWords;
+    int countTotalWords;
+ 
+    // declaring nodes
+    struct Node
+    {
+        string word;
+        Node *next;
+        Node(const string &w)
+        {
+            word = w;        
+            next = nullptr;  
+        }
+
+    };
+
+    Node *head;
 
 
 };
