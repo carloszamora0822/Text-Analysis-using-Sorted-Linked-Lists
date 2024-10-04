9757bb6fba56a357e6dddf34171d308b44c8efdf
Carlos Zamora <165355838+carloszamora0822@users.noreply.github.com>
2024-10-04 13:08:57 -0500

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -6,57 +6,101 @@
 using namespace std;
 
 // constructor
-
-LinkedList::LinkedList()
+LinkedList::LinkedList() 
 {
+    head = nullptr; 
+    countCleanedWords = 0; 
+    countTotalWords = 0; 
+
 }
 
 // destructor
 LinkedList::~LinkedList()
 {
+    Node *current = head;
+    while (current != nullptr) 
+    {
+        Node *nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
 }
 
-bool LinkedList::isWord(string &word)
+void LinkedList::insertSorted(const string &word) 
 {
-    for (int i = 0; i < word.length(); i++) 
+    // duplicate check
+    Node *current = head;
+    while (current != nullptr) 
     {
-        if (!isalpha(word[i])) 
+        if (current->word == word) 
         {
-            return false;  
+            return;
         }
-
-        else
-            word[i] = tolower(word[i]);
+        current = current->next;
     }
-    
-    return true;
-}   
+
+    Node *newNode = new Node(word);
+
+    if (head == nullptr || word < head->word) 
+    {
+        countCleanedWords++;
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node *prev = nullptr;
+    current = head;
+    while (current != nullptr && current->word < word) 
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    newNode->next = prev->next;
+    prev->next = newNode;
+    countCleanedWords++;
+}
 
 void LinkedList::read_txt(ifstream &inputfile)
 {
-    vector <string> validWords;
     string word;
 
     while(inputfile >> word)
     {
-        if(isWord(word))
+        string cleanedWord = "";
+
+        countTotalWords+=1;
+
+        for (int i = 0; i < word.length(); i++) 
         {
-            validWords.push_back(word);
+            if(isalpha(word[i]) || word[i] == '-' || word[i] == '\'') 
+            {
+                cleanedWord = cleanedWord + char(tolower(word[i]));
+            }
+
         }
 
-        else
-            continue;
+        if (!cleanedWord.empty()) 
+        {
+            insertSorted(cleanedWord);
+        }
     }
-
-    for(int i = 0; i < validWords.size(); i++)
-    {
-        cout << validWords[i] << endl;
-    }
-
-
 }
 
+void LinkedList::printList() const 
+{
+    Node *current = head;
+    while (current != nullptr) 
+    {
+        cout << current->word << endl;
+        current = current->next;
+    }
+}
 
-
-
-
+void LinkedList::output() const
+{
+    cout << endl;
+    cout << "Total unique words: " << countCleanedWords << endl;
+    cout << "Total words: " << countTotalWords << endl;
+}
